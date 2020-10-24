8e695e48fc2bbf830ad44462441c15660a23ec7f
BogdanCrs <crusoveanubogdan4@gmail.com>
2020-10-24 14:23:06 +0300

    Added the Clarifai API call  into the API and do validation for register and signin

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: 'd500f09d67ec41ae8d1acf62c390e857',
+});
+
+const handleApiCall = () => (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('Unable to work with API'));
+};
+
 const handleImage = (db) => (req, res) => {
   const { id } = req.body;
   db('users')
@@ -14,4 +29,5 @@ const handleImage = (db) => (req, res) => {
 
 module.exports = {
   handleImage: handleImage,
+  handleApiCall: handleApiCall,
 };
